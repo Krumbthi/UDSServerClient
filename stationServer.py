0db84dfb265580a8953d7edba57ece7d9021a04c
thilo <t.krumbholz@brabender-mt.de>
2019-04-02 14:52:25 +0200

    refactoring

--- stationServer.py
+++ stationServer.py
@@ -10,9 +10,20 @@ Logger = logging.getLogger(__name__)
 
 class StationServer:
 
-    def __init__(self):
+    def __init__(self, serverAddr):
         self.Logger = logging.getLogger("StationServer")
         self.Logger.debug("Init StationServer")
+        self.cMessage = dict()
+        self.Logger.debug("MyTCPServer init")
+        # Create a UDS socket
+        self.Sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+
+        # Bind the socket to the port
+        self.Logger.debug("starting up on %s" % serverAddr)
+        self.Sock.bind(serverAddr)
+
+        # Listen for incoming connections
+        self.Sock.listen(1)
 
         self.serverStack = dict()
         self.errorStack = []
@@ -34,69 +45,38 @@ class StationServer:
         self.Logger.debug("dataAnalyse cMessage %s" % cMessage)
         return cMessage
 
+    def serveForever(self):
+        while True:
+            # Wait for a connection
+            self.Logger.debug('waiting for a connection')
+            connection, client_address = self.Sock.accept()
+            try:
+                self.Logger.debug('connection from {}'.format(client_address))
+
+                # Receive the data in small chunks and retransmit it
+                while True:
+                    data = connection.recv(4096)
+                    self.Logger.debug("received \"{}\"".format(data))
+
+                    # if data received handle them
+                    if data:
+                        cMessage = json.loads(data.decode('utf-8').strip())
+                        calcData = self.dataAnalyse(cMessage)
+                        jsd = json.dumps(calcData, cls=DecimalEncoder).encode("utf-8")
+                        connection.sendall(jsd)
+                    else:
+                        self.Logger.debug("no more data from {}".format(client_address))
+                        break
+            finally:
+                # Clean up the connection
+                connection.close()
+
 
 class DecimalEncoder(json.JSONEncoder):
     def default(self, o):
         if isinstance(o, decimal.Decimal):
             return float(o)
         return super(DecimalEncoder, self).default(o)
-
-
-class MyTCPHandler:
-    def __init__(self):
-        self.cMessage = dict()
-        self.stS = StationServer()
-        self.Logger = logging.getLogger("MyTCPHandler")
-        self.Logger.debug("MyTCPHandler")
-
-    def handle(self, request):
-        cMessage = json.loads(request.strip())
-        self.Logger.debug(type(cMessage))
-
-        calcData = self.stS.dataAnalyse(cMessage)
-
-        jsd = json.dumps(calcData, cls=DecimalEncoder).encode("utf-8")
-        #self.request.send(jsd)
-        return jsd
-
-
-class MyServer:
-    def __init__(self, serverAddr):
-        Logger.debug("MyTCPServer init")
-        # Create a UDS socket
-        self.Sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-
-        # Bind the socket to the port
-        Logger.debug("starting up on %s" % serverAddr)
-        self.Sock.bind(serverAddr)
-
-        # Listen for incoming connections
-        self.Sock.listen(1)
-
-    def serveForever(self):
-        sockHandler = MyTCPHandler()
-
-        while True:
-            # Wait for a connection
-            Logger.debug('waiting for a connection')
-            connection, client_address = self.Sock.accept()
-            try:
-                Logger.debug('connection from {}'.format(client_address))
-
-                # Receive the data in small chunks and retransmit it
-                while True:
-                    data = connection.recv(4096)
-                    Logger.debug("received \"{}\"".format(data))
-                    if data:
-                        req = sockHandler.handle(data.decode('utf-8'))
-                        connection.sendall(req)
-                    else:
-                        Logger.debug("no more data from {}".format(client_address))
-                        break
-
-            finally:
-                # Clean up the connection
-                connection.close()
 
 
 if __name__ == "__main__":
@@ -109,5 +89,5 @@ if __name__ == "__main__":
         if os.path.exists(server_address):
             raise
 
-    server = MyServer(server_address)
+    server = StationServer(server_address)
     server.serveForever()
