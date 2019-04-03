4a12a4a24559b6e4dea7fcc58a08dfa71cedf602
thilo <t.krumbholz@brabender-mt.de>
2019-04-03 15:40:59 +0200

    play arounds

--- client.py
+++ client.py
@@ -3,52 +3,99 @@ import sys
 import json
 import logging
 import time
+from decimal import Decimal
 
-
+# ----------------------------------------------------------------------------------------------------------------------
+# Logging
+# ----------------------------------------------------------------------------------------------------------------------
 logging.basicConfig(format='%(asctime)s %(message)s', level=logging.DEBUG)
 Logger = logging.getLogger(__name__)
 
+STABLE = 1
 
+# ----------------------------------------------------------------------------------------------------------------------
+# dispatcher
+# ----------------------------------------------------------------------------------------------------------------------
+def dispatch_data(data):
+    js_data = json.loads(data.decode('utf-8').strip())
+    js_data['a'] -= 1
+    js_data['b'] -= 1
+    js_data['c'] -= 1
+    return js_data
+
+
+def recvData(message):
+    server_address = './uds_socket'
+    try:
+        with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as sock:
+            sock.connect(server_address)
+            jSd = json.dumps(message).encode("utf-8")
+            # print("Client to Server %s" % jSd)
+            sock.send(jSd)
+            receivedData = sock.recv(4069)
+            received = json.loads(receivedData)
+
+            if received is None:
+                return None
+
+    except socket.error as msg:
+        Logger.debug("Server error: {}".format(msg))
+        return None
+
+    return received
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Main loop
+# ----------------------------------------------------------------------------------------------------------------------
 def main():
     counter = 0
-    # Create a UDS socket
-    sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-
-    # Connect the socket to the port where the server is listening
-    server_address = './uds_socket'
-    Logger.debug("connecting to %s" % server_address)
-
-    try:
-        sock.connect(server_address)
-    except socket.error as msg:
-        Logger.debug(msg)
-        sys.exit(1)
     message = dict()
 
-    try:
-        # Send data
-        message['a'] = "testa"
-        message['b'] = "testb"
-        message['c'] = "testc"
+    # Send data
+    message['a'] = 0
+    message['b'] = 0
+    message['c'] = 0
 
+    if STABLE:
+        # Create a UDS socket
+        sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+
+        # Connect the socket to the port where the server is listening
+        server_address = './uds_socket'
+        Logger.debug("connecting to %s" % server_address)
+
+        try:
+            sock.connect(server_address)
+        except socket.error as msg:
+            Logger.debug(msg)
+            sys.exit(1)
+
+        try:
+            while counter <= 10:
+                Logger.debug("sending \"%s\"" % message)
+                sock.sendall(json.dumps(message).encode("utf-8"))
+
+                amount_received = 0
+                amount_expected = len(message)
+
+                while amount_received < amount_expected:
+                    data = sock.recv(4096)
+                    amount_received += len(data)
+                    Logger.debug("received \"%s\"" % data)
+                    #message.update(dispatch_data(data))
+                    counter += 1
+
+                time.sleep(2)
+
+        finally:
+            Logger.debug('closing socket')
+            sock.close()
+    else:
         while counter <= 10:
-            Logger.debug("sending \"%s\"" % message)
-            sock.sendall(json.dumps(message).encode("utf-8"))
-
-            amount_received = 0
-            amount_expected = len(message)
-
-            while amount_received < amount_expected:
-                data = sock.recv(4096)
-                amount_received += len(data)
-                Logger.debug("received \"%s\"" % data)
-                counter += 1
-
+            Logger.debug(recvData(message))
+            counter += 1
             time.sleep(2)
-
-    finally:
-        Logger.debug('closing socket')
-        sock.close()
 
 
 if __name__ == "__main__":
--- stationServer.py
+++ stationServer.py
@@ -2,19 +2,61 @@ import decimal
 import json
 import socket
 import os
+import sys
+from threading import Timer
 import logging
 
+# ----------------------------------------------------------------------------------------------------------------------
+# Logging
+# ----------------------------------------------------------------------------------------------------------------------
 logging.basicConfig(format='%(asctime)s %(name)s %(message)s', level=logging.DEBUG)
 Logger = logging.getLogger(__name__)
 
 
-class StationServer:
+# ----------------------------------------------------------------------------------------------------------------------
+# Timer Class
+# ----------------------------------------------------------------------------------------------------------------------
+class GlobalTimer(object):
+    def __init__(self, interval=1.0):
+        self._registered = set()
+        self._interval = interval
+        self._timer = None
+        self._start_timer()
 
+    def register_callback(self, callback):
+        self._registered.add(callback)
+
+    def unregister_callback(self, callback):
+        self._registered.remove(callback)
+
+    def _start_timer(self):
+        self._timer = Timer(self._interval, self._callback)
+        self._timer.start()
+
+    def _callback(self):
+        for callback in self._registered:
+            callback()
+        self._start_timer()
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Global timer
+# ----------------------------------------------------------------------------------------------------------------------
+Scheduler = GlobalTimer(0.5)
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Server
+# ----------------------------------------------------------------------------------------------------------------------
+class Server():
     def __init__(self, serverAddr):
         self.Logger = logging.getLogger("StationServer")
         self.Logger.debug("Init StationServer")
-        self.cMessage = dict()
-        self.Logger.debug("MyTCPServer init")
+        self.cMessage = {'a': 0, 'b': 0, 'c': 0}
+        self.Connection = None
+
+        # Test Timer
+        Scheduler.register_callback(self.getLiveData)
+
         # Create a UDS socket
         self.Sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 
@@ -37,48 +79,57 @@ class StationServer:
         if 'liveMeasurement' not in self.serverStack:
             self.serverStack['liveMeasurement'] = []
 
-    def getLiveData(self, cMessage):
-        self.Logger.debug("get LiveData cMessage %s" % cMessage)
+    def getLiveData(self):
+        self.Logger.debug("get LiveData cMessage %s" % self.cMessage)
+        self.cMessage['a'] += 1
+        self.cMessage['b'] += 1
+        self.cMessage['c'] += 1
 
-    def dataAnalyse(self, cMessage):
-        cMessage['a'] = 'resA'
-        self.Logger.debug("dataAnalyse cMessage %s" % cMessage)
-        return cMessage
+    def dataAnalyse(self, data):
+
+        self.cMessage['a'] = data['a']
+        self.cMessage['b'] = data['b']
+        self.cMessage['c'] = data['c']
+
+        self.Logger.debug("dataAnalyse cMessage %s" % self.cMessage)
+        self.getLiveData()
+
+        jsd = json.dumps(self.cMessage).encode("utf-8")
+        self.Connection.sendall(jsd)
+
+        #return data
 
     def serveForever(self):
         while True:
             # Wait for a connection
             self.Logger.debug('waiting for a connection')
-            connection, client_address = self.Sock.accept()
+            self.Connection, client_address = self.Sock.accept()
             try:
                 self.Logger.debug('connection from {}'.format(client_address))
 
                 # Receive the data in small chunks and retransmit it
                 while True:
-                    data = connection.recv(4096)
+                    data = self.Connection.recv(4096)
                     self.Logger.debug("received \"{}\"".format(data))
 
                     # if data received handle them
                     if data:
-                        cMessage = json.loads(data.decode('utf-8').strip())
-                        calcData = self.dataAnalyse(cMessage)
-                        jsd = json.dumps(calcData, cls=DecimalEncoder).encode("utf-8")
-                        connection.sendall(jsd)
+                        js_data = json.loads(data.decode('utf-8').strip())
+                        #self.dataAnalyse(js_data)
+                        #jsd = json.dumps(self.cMessage).encode("utf-8")
+                        #connection.sendall(jsd)
                     else:
                         self.Logger.debug("no more data from {}".format(client_address))
                         break
+
             finally:
                 # Clean up the connection
-                connection.close()
+                self.Connection.close()
 
 
-class DecimalEncoder(json.JSONEncoder):
-    def default(self, o):
-        if isinstance(o, decimal.Decimal):
-            return float(o)
-        return super(DecimalEncoder, self).default(o)
-
-
+# ----------------------------------------------------------------------------------------------------------------------
+# Main loop
+# ----------------------------------------------------------------------------------------------------------------------
 if __name__ == "__main__":
     server_address = './uds_socket'
 
@@ -89,5 +140,12 @@ if __name__ == "__main__":
         if os.path.exists(server_address):
             raise
 
-    server = StationServer(server_address)
-    server.serveForever()
+    server = Server(server_address)
+    try:
+        server.serveForever()
+    except KeyboardInterrupt:
+        Scheduler.unregister_callback(server.getLiveData)
+    finally:
+        Logger.debug("Server died smoothly")
+
+    sys.exit(0)
